a562734bd015265e03baed7f0e2eb6e777b14878
Kirill Makhnovskiy <script.makhnovskiy@gmail.com>
2024-01-26 12:16:52 +0200

    add structure and api

---
+++ coiner/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import homeStore from '../stores/homeStore'
+
+export default function Home() {
+    const store = homeStore();
+
+    React.useEffect(() => {
+        store.fetchCoins(); 
+    }, [])
+
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
